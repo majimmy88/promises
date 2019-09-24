32a278846733fe45659e14bd948b4bf9dbfd8125
majimmy88 <majimmy88@gmail.com>
2019-09-24 15:36:34 -0700

    finished promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,12 +10,58 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
+var promiseConstructors = require('./promiseConstructor.js');
+var promisification = require('./promisification.js');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  return promiseConstructors.pluckFirstLineFromFileAsync(readFilePath)
+    .then((user) => promisification.getGitHubProfileAsync(user))
+    .then((profile) => fs.writeFileSync(writeFilePath,JSON.stringify(profile)));
+  // return new Promise ((resolve, request) => {
+  //   fs.readFile(readFilePath, 'utf8', (err, content) => {
+  //     if (err) {
+  //       reject (err);
+  //     } else {
+  //       var arr = content.split('\n');
+  //       resolve (arr[0]);
+  //     }
+  //   });
+  // })
+  //   .then( (user) => {
+  //     return new Promise((resolve,reject) => {
+  //       var options = {
+  //         url: 'https://api.github.com/users/' + user,
+  //         headers: { 'User-Agent': 'request' },
+  //         json: true  // will JSON.parse(body) for us
+  //       };
+  //       request.get(options, function (err, res, body) {
+  //         if (err) {
+  //           reject(err)
+  //         } else if (body.Message) {
+  //           reject(new Error('Failed'))
+  //         } else {
+  //           resolve(body);
+  //         }
+  //       });
+  //     });
+  //   })
+  //   .then((profile) => {
+  //     return new Promise((resolve,reject) => {
+  //       fs.writeFile(writeFilePath, JSON.stringify(profile), (err) => {
+  //         if (err) {
+  //           reject(err);
+  //         } else {
+  //           resolve(profile);
+  //         }
+  //       });
+  //     });
+  //   });
 };
+
 
 // Export these functions so we can test them
 module.exports = {
